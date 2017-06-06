9de7efbd65fb006195f9edb5e1e434c294807a0d
Nam Bui <nambv@dwarvesf.com>
2017-06-06 23:15:14 +0700

    Edit scope activity

--- app/src/main/java/dagger/nambv/app/demo/di/scope/ActivityScope.kt
+++ app/src/main/java/dagger/nambv/app/demo/di/scope/ActivityScope.kt
@@ -1,11 +1,6 @@
 package dagger.nambv.app.demo.di.scope
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
 import javax.inject.Scope
 
-@Suppress("DEPRECATED_JAVA_ANNOTATION")
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
 annotation class ActivityScope
